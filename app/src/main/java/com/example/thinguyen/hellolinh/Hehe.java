e6fdeb6c14e8fdc5c5718aea6a0ad203bf0408c9
nguyenlinh <linh.nguyen93dhv@gmail.com>
2017-06-29 11:44:11 +0700

    kaka

---
+++ app/src/main/java/com/example/thinguyen/hellolinh/Hehe.java
@@ -0,0 +1,22 @@
+package com.example.thinguyen.hellolinh;
+
+/**
+ * Created by nguyenlinh on 29/06/2017.
+ */
+
+public class Hehe {
+    int kaka;
+
+    public int getKaka() {
+        return kaka;
+    }
+
+    public void setKaka(int kaka) {
+        this.kaka = kaka;
+    }
+
+    public Hehe(int kaka) {
+
+        this.kaka = kaka;
+    }
+}
